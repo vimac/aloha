e126a756353ee956307b545910540ce53917313e
vimac <vifix.mac@gmail.com>
2018-07-07 16:23:21 +0800

    fix UT

--- test/core/test_server_client.js
+++ test/core/test_server_client.js
@@ -2,6 +2,7 @@ const GRPCServer = require('../../src/core/grpc_server');
 const createGRPCClient = require('../../src/core/grpc_client');
 
 const grpc = require('grpc');
+const protoLoader = require('@grpc/proto-loader');
 
 const assert = require('assert');
 
@@ -11,14 +12,15 @@ const createTestServerAndClient = (serverImpl) => {
   assert.notEqual(serverImpl.doSomeTest, null, 'serverImpl should implement the method "doSomeTest"');
 
   let port = parseInt(Math.random() * 2000 + 63000);
-  let proto = grpc.load(__dirname + '/../share/test.proto');
+  let proto = protoLoader.loadSync('test.proto', {includeDirs: [__dirname + '/../share/']});
+  let package = grpc.loadPackageDefinition(proto);
 
   let server = new GRPCServer('127.0.0.1', port);
-  server.bind(proto['TestService'].service, serverImpl);
+  server.bind(package['TestService'].service, serverImpl);
 
-  let clientWrapper = createGRPCClient('127.0.0.1', port, proto['TestService']);
+  let clientWrapper = createGRPCClient('127.0.0.1', port, package['TestService']);
 
-  return {proto, server, clientWrapper};
+  return {package, server, clientWrapper};
 
 }
 
